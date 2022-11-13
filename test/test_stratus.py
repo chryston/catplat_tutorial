b135bf7dc5e77e84c9c02f75a12861ba71a2a687
benjaminchen22 <benjamin_chen@ihpc.a-star.edu.sg>
2022-11-13 14:00:43 +0800

    added: test_stratus scriptg

---
+++ test/test_stratus.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+import shlex
+def run_command():
+
+class TestStratusSetup(object):
+    def test_setup(self):
+        command = 'source /apps/anaconda3-individual-edition/install/etc/profile.d/conda.sh'
